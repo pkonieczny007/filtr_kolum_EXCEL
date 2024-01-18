92aff8634ba1a17be18f4a9ca290fd184f8e3fd7
pkonieczny007 <50171535+pkonieczny007@users.noreply.github.com>
2024-01-18 10:21:38 +0100

    Delete req.py

--- req.py
+++
@@ -1,9 +0,0 @@
-required_modules = [
-    "pandas",
-    "openpyxl",
-    "requests"
-]
-
-with open('requirements.txt', 'w') as file:
-    for module in required_modules:
-        file.write(module + '\n')
